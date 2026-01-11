77aed2fc09d86bdf8231ab7bd72defd89b8b3166
Akshat <70469986+akshatshah24@users.noreply.github.com>
2026-01-11 17:07:24 -0500

    Time: 4 ms (93.87%), Space: 46.3 MB (100%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        if (n == 0) return 0;
+        int m = matrix[0].length;
+        int[] heights = new int[m];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
+            }
+            ans = Math.max(ans, largestRectangleArea(heights));
+        }
+        return ans;
+    }
+
+    private int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        if (n == 0) return 0;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = -1;
+        right[n - 1] = n;
+        for (int i = 1; i < n; i++) {
+            int p = i - 1;
+            while (p >= 0 && heights[p] >= heights[i]) {
+                p = left[p];
+            }
+            left[i] = p;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            int p = i + 1;
+            while (p < n && heights[p] >= heights[i]) {
+                p = right[p];
+            }
+            right[i] = p;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, heights[i] * (right[i] - left[i] - 1));
+        }
+        return ans;
+    }
+}
