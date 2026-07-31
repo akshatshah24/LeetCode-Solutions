c3747216bab5a6c0ff066b6e74fe1998ff5f7cdc
Akshat <70469986+akshatshah24@users.noreply.github.com>
2026-07-30 21:00:01 -0400

    Time: 9 ms (94.22%), Space: 48 MB (73.7%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int[] freq = new int[26];
+
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Arrays.sort(freq);
+        int ans = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) continue;
+            ans += (((25 - i) / 8) + 1) * freq[i];
+        }
+        return ans;
+    }
+}
